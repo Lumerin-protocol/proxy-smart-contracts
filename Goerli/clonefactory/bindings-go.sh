8e0f349c99754b8da56687278fe4ce0f73c8cae3
Oleksandr Shevchuk <shev@titan.io>
2022-10-07 19:27:57 +0200

    debug

--- Goerli/clonefactory/bindings-go.sh
+++ Goerli/clonefactory/bindings-go.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 rm -rf bindings-go
 mkdir -p bindings-go/{clonefactory,implementation,lumerintoken}
+ls -la
+ls -la abi
+ls -la bindings-go
 abigen --abi=./abi/CloneFactory.json --pkg=clonefactory --out=./bindings-go/clonefactory/clonefactory.go
 abigen --abi=./abi/Implementation.json --pkg=implementation --out=./bindings-go/implementation/implementation.go
 abigen --abi=./abi/Lumerin.json --pkg=lumerintoken --out=./bindings-go/lumerintoken/lumerintoken.go
