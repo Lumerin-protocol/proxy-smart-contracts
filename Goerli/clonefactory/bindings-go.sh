065a382b9dfb4dc54b38368688fe1645dd195f76
Oleksandr Shevchuk <shev@titan.io>
2022-10-10 15:28:17 +0200

    fix: avoid overwriting PATH

--- Goerli/clonefactory/bindings-go.sh
+++ Goerli/clonefactory/bindings-go.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-PATH="./bindings-go"
-rm -rf $PATH
+PTH="./bindings-go"
+rm -rf "$PTH"
 
-mkdir -p $PATH/clonefactory
-mkdir -p $PATH/implementation
-mkdir -p $PATH/lumerintoken
+mkdir -p "$PTH/clonefactory"
+mkdir -p "$PTH/implementation"
+mkdir -p "$PTH/lumerintoken"
 
 ls -la
 ls -la abi
-ls -la $PATH
-abigen --abi=./abi/CloneFactory.json --pkg=clonefactory --out=./$PATH/clonefactory/clonefactory.go
-abigen --abi=./abi/Implementation.json --pkg=implementation --out=./$PATH/implementation/implementation.go
-abigen --abi=./abi/Lumerin.json --pkg=lumerintoken --out=./$PATH/lumerintoken/lumerintoken.go
+ls -la $PTH
+abigen --abi=./abi/CloneFactory.json --pkg=clonefactory --out=./$PTH/clonefactory/clonefactory.go
+abigen --abi=./abi/Implementation.json --pkg=implementation --out=./$PTH/implementation/implementation.go
+abigen --abi=./abi/Lumerin.json --pkg=lumerintoken --out=./$PTH/lumerintoken/lumerintoken.go
