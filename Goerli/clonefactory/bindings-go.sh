a2e665248a40f0129cc55d3b816bdeda2dd26aaf
Oleksandr Shevchuk <shev@titan.io>
2022-10-19 17:18:56 +0200

    feat: rename to contracts-go and contracts-js, deploy contracts-js to github repo

--- Goerli/clonefactory/bindings-go.sh
+++ Goerli/clonefactory/bindings-go.sh
@@ -6,9 +6,6 @@ mkdir -p "$PTH/clonefactory"
 mkdir -p "$PTH/implementation"
 mkdir -p "$PTH/lumerintoken"
 
-ls -la
-ls -la abi
-ls -la $PTH
 abigen --abi=./abi/CloneFactory.json --pkg=clonefactory --out=./$PTH/clonefactory/clonefactory.go
 abigen --abi=./abi/Implementation.json --pkg=implementation --out=./$PTH/implementation/implementation.go
 abigen --abi=./abi/Lumerin.json --pkg=lumerintoken --out=./$PTH/lumerintoken/lumerintoken.go
