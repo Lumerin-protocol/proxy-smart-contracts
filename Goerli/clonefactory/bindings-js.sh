a9d98d5c210abc46cc4783866e9e71a0c41105c6
Oleksandr Shevchuk <shev@titan.io>
2022-10-19 21:06:26 +0200

    debug

--- Goerli/clonefactory/bindings-js.sh
+++ Goerli/clonefactory/bindings-js.sh
@@ -4,14 +4,15 @@ echo $PTH
 rm -rf $PTH
 mkdir -p $PTH/src/abi
 
-ls -la
-
 cp -r ./templates/js/ $PTH
 cp ./abi/CloneFactory.json $PTH/src/abi
 cp ./abi/Implementation.json $PTH/src/abi
 cp ./abi/Lumerin.json $PTH/src/abi
 
 cd $PTH
+
+ls -la
+
 yarn
 
 # build typing from abi
