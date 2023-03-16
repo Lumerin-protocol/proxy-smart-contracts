6dea799a0873184682efc48cf8a3bbce7fd81db5
Scott Terry <srt0422@gmail.com>
2023-03-15 20:55:51 -0700

    switch from localhost network to hardhat network for local deployment

--- Goerli/clonefactory/node-local-deploy.sh
+++ Goerli/clonefactory/node-local-deploy.sh
@@ -12,19 +12,19 @@ yarn wait-on http://127.0.0.1:8545 -l
 export CLONE_FACTORY_WHITELIST_ADDRESSES='["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"]'
 
 # deploy lumerin token
-yarn hardhat run --network localhost --config hardhat-base.config.js ./scripts/deploy-lumerin.js
+yarn hardhat run --network hardhat --config hardhat-base.config.js ./scripts/deploy-lumerin.js
 export LUMERIN_TOKEN_ADDRESS="$(cat lumerin-addr.tmp)"
 export VALIDATOR_ADDRESS="$(cat lumerin-addr.tmp)" # currently unused
 
 # deploy faucet
-yarn hardhat run --network localhost --config hardhat-base.config.js ./scripts/deploy-faucet.js 
+yarn hardhat run --network hardhat --config hardhat-base.config.js ./scripts/deploy-faucet.js 
 
 # deploy clonefactory
-yarn hardhat run --network localhost --config hardhat-base.config.js ./scripts/deploy-clonefactory-local.js
+yarn hardhat run --network hardhat --config hardhat-base.config.js ./scripts/deploy-clonefactory-local.js
 export CLONE_FACTORY_ADDRESS="$(cat clonefactory-addr.tmp)"
 
 # whitelist clonefactory addresses
-yarn hardhat run --network localhost --config hardhat-base.config.js ./scripts/whitelist-clonefactory.js
+yarn hardhat run --network hardhat --config hardhat-base.config.js ./scripts/whitelist-clonefactory.js
 
 # populate contracts
-yarn hardhat run --network localhost --config hardhat-base.config.js ./scripts/populate-contracts.js
+yarn hardhat run --network hardhat --config hardhat-base.config.js ./scripts/populate-contracts.js
