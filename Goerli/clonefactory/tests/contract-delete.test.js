28434bafe81c8fdce44cba59723f6662a5a88be9
Oleksandr Shevchuk <shev@titan.io>
2023-09-14 12:30:23 +0200

    fix: reorganized tests, split contract-closeout file

--- Goerli/clonefactory/tests/contract-delete.test.js
+++ Goerli/clonefactory/tests/contract-delete.test.js
@@ -1,16 +1,18 @@
 //@ts-check
 const { expect } = require("chai");
-const ethers  = require("hardhat");
+const ethers = require("hardhat");
 const Web3 = require("web3");
 const { Lumerin, CloneFactory, Implementation } = require("../build-js/dist")
+const { LocalTestnetAddresses } = require("./utils")
 
 describe("Contract delete", function () {
-  const lumerinAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
-  const cloneFactoryAddress = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
-
-  const owner = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-  const seller = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
-  const buyer = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
+  const {
+    lumerinAddress,
+    cloneFactoryAddress,
+    owner,
+    seller,
+    buyer,
+  } = LocalTestnetAddresses;
 
   /** @type {import("web3").default} */
   // @ts-ignore
@@ -19,16 +21,16 @@ describe("Contract delete", function () {
   let hrContractAddr = ""
   let fee = ""
 
-  before(async ()=>{
+  before(async () => {
     const lumerin = Lumerin(web3, lumerinAddress)
-    await lumerin.methods.increaseAllowance(cloneFactoryAddress, "10000").send({from: buyer})
-    await lumerin.methods.transfer(buyer, "10000").send({from: owner})
-    await cf.methods.setAddToWhitelist(seller).send({from: owner})
+    await lumerin.methods.increaseAllowance(cloneFactoryAddress, "10000").send({ from: buyer })
+    await lumerin.methods.transfer(buyer, "10000").send({ from: owner })
+    await cf.methods.setAddToWhitelist(seller).send({ from: owner })
     fee = await cf.methods.marketplaceFee().call()
   })
 
-  it("should create contract and check its status", async function(){
-    const receipt = await cf.methods.setCreateNewRentalContract("1", "0", "1", "3600", cloneFactoryAddress, "123").send({from: seller, value: fee})
+  it("should create contract and check its status", async function () {
+    const receipt = await cf.methods.setCreateNewRentalContract("1", "0", "1", "3600", cloneFactoryAddress, "123").send({ from: seller, value: fee })
     hrContractAddr = receipt.events?.contractCreated.returnValues._address;
     const impl = Implementation(web3, hrContractAddr)
     const data = await impl.methods.getPublicVariables().call()
@@ -36,93 +38,93 @@ describe("Contract delete", function () {
     expect(data._isDeleted).equal(false)
   })
 
-  it('should prohibit deletion if caller is not a seller', async function(){
-    try{
-      await cf.methods.setContractDeleted(hrContractAddr, true).send({from: buyer})
+  it('should prohibit deletion if caller is not a seller', async function () {
+    try {
+      await cf.methods.setContractDeleted(hrContractAddr, true).send({ from: buyer })
       expect.fail("should throw error")
-    } catch(e){
+    } catch (e) {
       expect(e.message).includes("you are not authorized")
     }
   })
 
-  it("should delete contract and emit event", async function(){
-    await cf.methods.setContractDeleted(hrContractAddr, true).send({from: seller})
+  it("should delete contract and emit event", async function () {
+    await cf.methods.setContractDeleted(hrContractAddr, true).send({ from: seller })
     const impl = Implementation(web3, hrContractAddr)
     const data = await impl.methods.getPublicVariables().call()
 
     expect(data._isDeleted).equal(true)
 
-    const events = await cf.getPastEvents("contractDeleteUpdated", {fromBlock: 0, toBlock: "latest"})
-    const isEventFound = events.find((e)=>
-      e.returnValues._address === hrContractAddr && 
+    const events = await cf.getPastEvents("contractDeleteUpdated", { fromBlock: 0, toBlock: "latest" })
+    const isEventFound = events.find((e) =>
+      e.returnValues._address === hrContractAddr &&
       e.returnValues._isDeleted === true
     )
 
     expect(isEventFound).not.undefined
   })
 
-  it("should error on second attempt to delete", async function(){
-    try{
-      await cf.methods.setContractDeleted(hrContractAddr, true).send({from: seller})
+  it("should error on second attempt to delete", async function () {
+    try {
+      await cf.methods.setContractDeleted(hrContractAddr, true).send({ from: seller })
       expect.fail("should throw error")
-    } catch(e){
+    } catch (e) {
       expect(e.message).includes("contract delete state is already set to this value")
     }
   })
 
-  it("should block purchase if contract deleted", async function(){
-    try{
-      await cf.methods.setPurchaseRentalContract(hrContractAddr, "abc").send({from: buyer, value: fee})
+  it("should block purchase if contract deleted", async function () {
+    try {
+      await cf.methods.setPurchaseRentalContract(hrContractAddr, "abc").send({ from: buyer, value: fee })
       expect.fail("should throw error")
-    } catch(e){
+    } catch (e) {
       expect(e.message).includes("cannot purchase deleted contract")
     }
   })
 
-  it("should undelete contract and emit event", async function(){
-    await cf.methods.setContractDeleted(hrContractAddr, false).send({from: seller})
+  it("should undelete contract and emit event", async function () {
+    await cf.methods.setContractDeleted(hrContractAddr, false).send({ from: seller })
     const impl = Implementation(web3, hrContractAddr)
     const data = await impl.methods.getPublicVariables().call()
 
     expect(data._isDeleted).equal(false)
 
-    const events = await cf.getPastEvents("contractDeleteUpdated", {fromBlock: 0, toBlock: "latest"})
+    const events = await cf.getPastEvents("contractDeleteUpdated", { fromBlock: 0, toBlock: "latest" })
     const isEventFound = events.find((e) =>
-      e.returnValues._address === hrContractAddr && 
+      e.returnValues._address === hrContractAddr &&
       e.returnValues._isDeleted === false
     )
 
     expect(isEventFound).not.undefined
   })
 
-  it("should allow purchase if contract undeleted", async function(){
-    await cf.methods.setPurchaseRentalContract(hrContractAddr, "abc").send({from: buyer, value: fee})
+  it("should allow purchase if contract undeleted", async function () {
+    await cf.methods.setPurchaseRentalContract(hrContractAddr, "abc").send({ from: buyer, value: fee })
   })
 
-  it("should allow delete contract if contract is purchased", async function(){
-    await cf.methods.setContractDeleted(hrContractAddr, true).send({from: seller})
+  it("should allow delete contract if contract is purchased", async function () {
+    await cf.methods.setContractDeleted(hrContractAddr, true).send({ from: seller })
     const impl = Implementation(web3, hrContractAddr)
     const data = await impl.methods.getPublicVariables().call()
 
     expect(data._isDeleted).equal(true)
   })
 
-  it("should prohibit deletion on the contract instance", async function(){
+  it("should prohibit deletion on the contract instance", async function () {
     const impl = Implementation(web3, hrContractAddr)
-    try{
-      await impl.methods.setContractDeleted(true).send({from: seller})
+    try {
+      await impl.methods.setContractDeleted(true).send({ from: seller })
       expect.fail("should throw error")
-    } catch(e){
+    } catch (e) {
       expect(e.message).includes("this address is not approved to call this function")
     }
   })
 
-  it("should allow deletion from clonefactory owner", async function(){
+  it("should allow deletion from clonefactory owner", async function () {
     const impl = Implementation(web3, hrContractAddr)
-    try{
-      await impl.methods.setContractDeleted(true).send({from: owner})
+    try {
+      await impl.methods.setContractDeleted(true).send({ from: owner })
       expect.fail("should throw error")
-    } catch(e){
+    } catch (e) {
       expect(e.message).includes("this address is not approved to call this function")
     }
   })
--- Goerli/clonefactory/tests/contract-update.test.js
+++ Goerli/clonefactory/tests/contract-update.test.js
@@ -1,20 +1,21 @@
 //@ts-check
 const { expect } = require("chai");
-const ethers  = require("hardhat");
+const ethers = require("hardhat");
 const Web3 = require("web3");
 const { Lumerin, CloneFactory, Implementation } = require("../build-js/dist");
-const { ToString } = require("./utils");
+const { ToString, LocalTestnetAddresses } = require("./utils");
 
 describe("Contract terms update", function () {
-  const lumerinAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
-  const cloneFactoryAddress = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
+  const {
+    lumerinAddress,
+    cloneFactoryAddress,
+    owner,
+    seller,
+    buyer,
+  } = LocalTestnetAddresses;
 
-  const owner = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-  const seller = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
-  const buyer = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
-
-  const price = ToString(1 * 10**8);
-  const newPrice = ToString(2 * 10**8);
+  const price = ToString(1 * 10 ** 8);
+  const newPrice = ToString(2 * 10 ** 8);
 
   /** @type {import("web3").default} */
   // @ts-ignore
@@ -23,17 +24,17 @@ describe("Contract terms update", function () {
   let hrContractAddr = ""
   let fee = ""
 
-  before(async ()=>{
+  before(async () => {
     const lumerin = Lumerin(web3, lumerinAddress)
-    await lumerin.methods.transfer(buyer, ToString(10* 10**8)).send({from: owner})
-    await lumerin.methods.increaseAllowance(cloneFactoryAddress, ToString(10* 10**8)).send({from: buyer})
-    await lumerin.methods.transfer(seller,  ToString(10* 10**8)).send({from: owner})
-    await cf.methods.setAddToWhitelist(seller).send({from: owner})
+    await lumerin.methods.transfer(buyer, ToString(10 * 10 ** 8)).send({ from: owner })
+    await lumerin.methods.increaseAllowance(cloneFactoryAddress, ToString(10 * 10 ** 8)).send({ from: buyer })
+    await lumerin.methods.transfer(seller, ToString(10 * 10 ** 8)).send({ from: owner })
+    await cf.methods.setAddToWhitelist(seller).send({ from: owner })
     fee = await cf.methods.marketplaceFee().call()
   })
 
-  it("should create contract and check its status", async function(){
-    const receipt = await cf.methods.setCreateNewRentalContract(price, "0", "1", "3600", cloneFactoryAddress, "123").send({from: seller, value: fee})
+  it("should create contract and check its status", async function () {
+    const receipt = await cf.methods.setCreateNewRentalContract(price, "0", "1", "3600", cloneFactoryAddress, "123").send({ from: seller, value: fee })
     hrContractAddr = receipt.events?.contractCreated.returnValues._address;
     const impl = Implementation(web3, hrContractAddr)
     const newData = await impl.methods.futureTerms().call()
@@ -46,17 +47,17 @@ describe("Contract terms update", function () {
     expect(data._price).equal(price);
   })
 
-  it('should prohibit updating if caller is not a seller', async function(){
-    try{
+  it('should prohibit updating if caller is not a seller', async function () {
+    try {
       await cf.methods.setUpdateContractInformation(hrContractAddr, newPrice, '1', '1', '1').send({ from: buyer })
       expect.fail("should throw error")
-    } catch(e){
+    } catch (e) {
       expect(e.message).includes("you are not authorized")
     }
   })
 
-  it("should update contract and emit event without futureTerms update", async function() {
-    const receipt = await cf.methods.setUpdateContractInformation(hrContractAddr, newPrice, '2', '3', '4').send({from: seller})
+  it("should update contract and emit event without futureTerms update", async function () {
+    const receipt = await cf.methods.setUpdateContractInformation(hrContractAddr, newPrice, '2', '3', '4').send({ from: seller })
 
     const impl = Implementation(web3, hrContractAddr)
     const futureTerms = await impl.methods.futureTerms().call()
@@ -68,8 +69,8 @@ describe("Contract terms update", function () {
     expect(data._speed).equal('3');
     expect(data._length).equal('4');
 
-    const events = await impl.getPastEvents('purchaseInfoUpdated', {fromBlock: receipt.blockNumber, toBlock: "latest"})
-    const isEventFound = events.find((e)=>
+    const events = await impl.getPastEvents('purchaseInfoUpdated', { fromBlock: receipt.blockNumber, toBlock: "latest" })
+    const isEventFound = events.find((e) =>
       e.returnValues._address === hrContractAddr
     )
 
@@ -77,11 +78,11 @@ describe("Contract terms update", function () {
   })
 
 
-  it("should store futureTerms for contract and should not emit update event if contract is running", async function(){
-    const price = ToString(2 * 10**8);
-    const newPrice = ToString(3 * 10**8);
-    await cf.methods.setPurchaseRentalContract(hrContractAddr, '').send({from: buyer, value: fee});
-    const receipt = await cf.methods.setUpdateContractInformation(hrContractAddr,  newPrice, '22', '33', '44').send({from: seller})
+  it("should store futureTerms for contract and should not emit update event if contract is running", async function () {
+    const price = ToString(2 * 10 ** 8);
+    const newPrice = ToString(3 * 10 ** 8);
+    await cf.methods.setPurchaseRentalContract(hrContractAddr, '').send({ from: buyer, value: fee });
+    const receipt = await cf.methods.setUpdateContractInformation(hrContractAddr, newPrice, '22', '33', '44').send({ from: seller })
     const impl = Implementation(web3, hrContractAddr)
     const futureTerms = await impl.methods.futureTerms().call()
     const data = await impl.methods.getPublicVariables().call()
@@ -97,18 +98,18 @@ describe("Contract terms update", function () {
     expect(data._length).equal('4');
 
 
-    const events = await impl.getPastEvents('purchaseInfoUpdated', {fromBlock: receipt.blockNumber, toBlock: "latest"})
-    const isEventFound = !!events.find((e)=>
+    const events = await impl.getPastEvents('purchaseInfoUpdated', { fromBlock: receipt.blockNumber, toBlock: "latest" })
+    const isEventFound = !!events.find((e) =>
       e.returnValues._address === hrContractAddr
     )
 
     expect(isEventFound).to.be.false;
   })
 
-  it("should apply futureTerms after contract closed and emit event", async function(){
-    const newPrice = ToString(3 * 10**8);
+  it("should apply futureTerms after contract closed and emit event", async function () {
+    const newPrice = ToString(3 * 10 ** 8);
     const impl = Implementation(web3, hrContractAddr)
-    const receipt = await impl.methods.setContractCloseOut("0").send({from: buyer})
+    const receipt = await impl.methods.setContractCloseOut("0").send({ from: buyer })
 
     const futureTerms = await impl.methods.futureTerms().call()
 
@@ -123,8 +124,8 @@ describe("Contract terms update", function () {
     expect(data._speed).equal('33');
     expect(data._length).equal('44');
 
-    const events = await impl.getPastEvents('purchaseInfoUpdated', {fromBlock: receipt.blockNumber, toBlock: "latest"})
-    const isEventFound = events.find((e)=>
+    const events = await impl.getPastEvents('purchaseInfoUpdated', { fromBlock: receipt.blockNumber, toBlock: "latest" })
+    const isEventFound = events.find((e) =>
       e.returnValues._address === hrContractAddr
     )
 
