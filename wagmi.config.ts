daeec1bebc8c7e26a7e9cc618ec6ce7cb0b38ae8
shev-titan <108101079+shev-titan@users.noreply.github.com>
2025-01-09 15:15:27 +0100

    ci: fix compile step (#16)
    Co-authored-by: Oleksandr Shevchuk <lsheva7@gmail.com>

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
       artifacts: "./artifacts/contracts",
       project: ".",
       exclude: ["*Test.sol/**", "vesting/**"],
+      commands: {
+        build: "yarn hardhat --config hardhat-base.config.ts compile",
+        rebuild: "yarn hardhat --config hardhat-base.config.ts compile",
+      },
     }),
   ],
   out: "./build-js/src/abi/abi.ts",
